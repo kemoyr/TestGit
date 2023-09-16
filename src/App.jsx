bec9db7bbc3a45d7aa6c98f416105e7af3f408d6
kemoyr <v15382636624@gmail.com>
2023-09-16 21:30:26 +0300

    add MainPage and NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Main from "./components/Main/Main";
+import NavBar from "./components/NavBar/NavBar";
 import RegForm from "./components/RegForm/RegForm";
 
 function App() {
 
   return (
     <>
+
+      <NavBar/>
+      <Main/>
       <RegForm />
 
     </>
